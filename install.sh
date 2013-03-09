f8d495d8728dd93e694c8102410e2c468a56029f
stas <stas@laptop>
2013-03-09 17:12:39 +0400

    Updated install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init
 rsync -avz --exclude-from=./.rsync_exclude ./ ~/
-apt-get install build-essential ruby
+apt-get install build-essential ruby ruby-dev
 cd ~/.vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
 make
