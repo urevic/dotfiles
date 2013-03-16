e3322b7ff5bfd9a18932cf85decebbb867c195a9
stas <stas@avt-project.ru>
2013-03-17 01:37:51 +0400

    Fixed bug in install

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 git submodule update --init
 rsync -avz --exclude-from=./.rsync_exclude ./ ~/
-rsync -avz ./.vim/bundle/ ~/.vim/bundle/
-rsync -avz ./.oh-my-zsh/custom/ ~/.oh-my-zsh/custom/
+rsync -avz --delete ./.vim/bundle/ ~/.vim/bundle/
+rsync -avz --delete ./.oh-my-zsh/custom/ ~/.oh-my-zsh/custom/
 apt-get install build-essential ruby ruby-dev
 cd ~/.vim/bundle/command-t/ruby/command-t
 ruby extconf.rb
