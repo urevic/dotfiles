16b42490dffef8573c6987f859bf9c7ad6a9c6a7
urevic <stas@avt-project.ru>
2018-02-13 14:53:56 +0200

    Add submodules update to install.sh and fix command-t compilation command

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git submodule update --init --recursive
+git submodule update --recursive --remote
 mkdir ./.vim/undodir
 mkdir -p ./.vim/tmp/backup
 mkdir -p ./.vim/tmp/swap
@@ -8,7 +9,6 @@ rsync -avz --exclude-from=./.rsync_exclude ./ ~/
 rsync -avz --delete ./.vim/bundle/ ~/.vim/bundle/
 rsync -avz --delete ./.oh-my-zsh/custom/ ~/.oh-my-zsh/custom/
 sudo apt-get install build-essential ruby ruby-dev
-cd ~/.vim/bundle/command-t/ruby/command-t
-ruby extconf.rb
-make
+cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t/
+make clean; ruby extconf.rb && make
 cd -
