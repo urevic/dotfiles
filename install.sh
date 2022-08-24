4c398864bf61e55b3080d89b26fbdb88c3d1c141
urevic <stas@avt-project.ru>
2022-08-24 16:04:03 +0300

    Fix a bug in install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+sudo apt-get install build-essential ruby ruby-dev rsync
 git submodule update --init --recursive
 git submodule update --recursive --remote
 echo "[!] python-mode should be installed separately as it uses submodules itself"
 cd ./.vim/bundle/
 git clone --recurse-submodules https://github.com/python-mode/python-mode.git
+cd -
 mkdir ./.vim/undodir
 mkdir -p ./.vim/tmp/backup
 mkdir -p ./.vim/tmp/swap
@@ -12,7 +14,6 @@ rsync -avz --exclude-from=./.rsync_exclude ./ ~/
 rsync -avz --delete ./.vim/bundle/ ~/.vim/bundle/
 rsync -avz --delete ./.oh-my-zsh/custom/ ~/.oh-my-zsh/custom/
 echo "Sudo for apt-get various buld packages"
-sudo apt-get install build-essential ruby ruby-dev
 cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t/
 make clean; ruby extconf.rb && make
 cd -
